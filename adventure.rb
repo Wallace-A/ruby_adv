cab93d63b4bd0c6be09d6b1dc7f62b2123ef4cbe
Andrew Wallace <andrew.wallace.english@gmail.com>
2021-01-21 16:59:37 +0900

    whey

--- adventure.rb
+++ adventure.rb
@@ -1,53 +1,61 @@
 require "./rooms"
-VERB_LIST = ["look", "move"]
+VERB_LIST = ["look at", "move"]
+DIRECTIONS = ["north", "south", "east", "west"]
 $current_room = nil
 # need enter room function?
 def display_message(message)
   puts message
 end
-
+#TODO: REQUEST VERB/OBJECT ARE SEPARATE FUNCTIONS THAT RETURN A VALUE TO SEND
 def request_input()
- display_message("What would you like to do? (Use look at + OBJECT)")
- player_input = gets.chomp
- #downcase and split input into array of words
- split_input = player_input.downcase.split
- #verb is first value in array
- verb = split_input.first
- object = split_input.last
+ display_message("What would you like to do? [LOOK AT|MOVE|PICK UP|USE]")
+ player_verb = gets.chomp.downcase
  #check if input is in list of verbs
- if(VERB_LIST.include?(verb))
-  #call verb method
-  send(verb, object)
+ if(VERB_LIST.include?(player_verb))
+  #ask for object to verb
+  puts "What would you like to #{player_verb}?"
+  player_object = gets.chomp.downcase
+  joined_player_verb = player_verb.split.join("_")
+  send(joined_player_verb, player_object)
   request_input
  else
-  display_message("Sorry, I don't understand '#{player_input}'")
+  display_message("Sorry, I don't understand '#{player_verb}'")
   request_input
  end
+
 end
+
 
 def enter_room(room)
-  $current_room = room
-  p "Entering #{$current_room["name"]}."
+  $current_room = $rooms[room]
+  "Entering #{$current_room["name"]}."
+  puts $current_room["look"]
 end
 
-def look(object)
-  if object == "look"
-    p "No object to look at."
+def look_at(object_name)
+  # TODO: resuable method for finding object to use
+  object = $current_room["objects"][object_name]
+  if object
+    puts object["description"]
   else
-    #p "looking at " + object
-    # get description
-    p $current_room["objects"][object]["description"]
+    puts "There is no #{object_name} to look at."
   end
 end
 
 def move(direction)
-  if direction == "move"
-    p "No object to move to."
+  #accepts directions of exits
+  if DIRECTIONS.include?(direction)
+    new_room = room = $current_room["exits"][direction]
+    if new_room
+      enter_room(room)
+    else
+      puts "Sorry, I can't go #{direction}"
+    end
   else
-    p "moving to " + direction
+    p "Can't move #{direction}"
   end
 end
-enter_room($test_room)
+enter_room("test_room")
 p $current_room["name"]
 #p $current_room["objects"]["table"]["description"]
 request_input()
--- rooms.rb
+++ rooms.rb
@@ -1,11 +1,24 @@
-$test_room = {
-  "name" => "test room",
-  "look" => "You are in a room.",
-  "objects" => {
-    "table" => {
-      "description" => "The table is made from rotting wood."
-    }
+$rooms = {
+ "test_room" => {
+    "name" => "test room",
+    "look" => "You are in a room.",
+    "objects" => {
+      "table" => {
+        "description" => "The table is made from rotting wood."
+      },
+    },
+    "exits" => {"north" => "north_room", "west" => "west_room"}
+  },
+
+  "north_room" => {
+    "name" => "north room",
+    "look" => "You are in north room.",
+    "objects" => {
+      "table" => {
+        "description" => "The table is made from rotting wood."
+      },
+    },
+    "exits" => {"south" => "test_room", "west" => "west_room"}
   }
 }
-
 $test_thing = "what?"
